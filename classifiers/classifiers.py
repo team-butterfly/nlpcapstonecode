79862c5b9925d65f9deef88d38e8036d6bbfa9d4
Brandon Vincent <brandv2@attu2.cs.washington.edu>
2017-04-18 13:50:46 -0700

    test_unigram uses TweetsDataSource

--- classifiers/classifiers.py
+++ classifiers/classifiers.py
@@ -28,18 +28,23 @@ class UnigramClassifier(Classifier):
     """
     UnigramClassifier classifies sentences by a simple bag-of-words model.
     """
-    def __init__(self):
-        pass
+    def __init__(self, num_labels, unk_threshold):
+        self._num_labels = num_labels
+        self._unk_threshold = unk_threshold
 
     def _encode_sentences(self, sentences):
         """Convert a sequence of sentences into a matrix of unigram counts."""
         return np.array([self._stringstore.count_vector(sent) for sent in sentences])
 
-    def train(self, inputs, true_labels, num_labels):
+    def train(self, inputs, true_labels, max_epochs=1000):
         """
         inputs: training inputs, a sequence of sentences
         true_labels: integer labels for each sentence
         num_labels: total number of labels for classification
+
+        Mandatory keyword args:
+        unk_threshold: if a token appears less than this many times, it is not added to the classifer's vocabulary
+        max_epochs: maximum number of training epochs to run
         """
         # First, set up the vocabulary and such
         def word_iter():
@@ -47,16 +52,15 @@ class UnigramClassifier(Classifier):
                 for token in sent:
                     yield token
 
-        self._stringstore = StringStore(word_iter(), unk_threshold=2)
+        self._stringstore = StringStore(word_iter(), unk_threshold=self._unk_threshold)
         self._vocab_size = len(self._stringstore)
-        self._num_labels = num_labels
 
-        console.log("UnigramClassifier.train: vocab:", self._stringstore.vocab())
+        # console.log("UnigramClassifier.train: vocab:", self._stringstore.vocab())
         console.log("UnigramClassifier.train: vocabulary size:", self._vocab_size)
         console.log("UnigramClassifier.train: num labels:", self._num_labels)
-        console.log("UnigramClassifier.train: raw inputs:\n", inputs)
-        console.log("UnigramClassifier.train: encoded inputs:\n", self._encode_sentences(inputs))
-        console.log("UnigramClassifier.train: true labels:\n", true_labels)
+        # console.log("UnigramClassifier.train: raw inputs:\n", inputs)
+        # console.log("UnigramClassifier.train: encoded inputs:\n", self._encode_sentences(inputs))
+        # console.log("UnigramClassifier.train: true labels:\n", true_labels)
 
         # Build the Tensorflow computation graph
         self._graph = tf.Graph()
@@ -98,15 +102,20 @@ class UnigramClassifier(Classifier):
             converged = False
             
             while not converged:
-                t += 1
                 x_batch, y_batch = self._encode_sentences(inputs), true_labels
                 train_feed = {
                     self._inputs: x_batch,
                     self._true_labels: y_batch
                 }
                 [_, cur_loss] = sess.run([self._train_op, self._loss], train_feed)
+                t += 1
+                
+                if t >= max_epochs:
+                    break
+
                 avg_loss_1 += cur_loss
                 if t > 0 and t % avg_steps == 0:
+                    console.log("Finished step", t)
                     avg_loss_1 /= avg_steps
                     converged = np.isclose(avg_loss_0, avg_loss_1, rtol=1e-04, atol=1e-05)
                     avg_loss_0, avg_loss_1 = avg_loss_1, 0
@@ -115,9 +124,9 @@ class UnigramClassifier(Classifier):
             self._w_saved = self._w.eval()
             self._b_saved = self._b.eval()
 
-            console.log("UnigramClassifier.train: converged in {} steps.".format(t))
-            console.log("UnigramClassifier.train: weights\n", self._w_saved)
-            console.log("UnigramClassifier.train: biases\n", self._b_saved)
+            console.log("UnigramClassifier.train: terminated training after {} steps.".format(t))
+            # console.log("UnigramClassifier.train: weights\n", self._w_saved)
+            # console.log("UnigramClassifier.train: biases\n", self._b_saved)
 
     def predict(self, sentences):
         console.log("UnigramClassifier.predict: text\n",
@@ -130,5 +139,4 @@ class UnigramClassifier(Classifier):
                 self._b: self._b_saved
             }
             predictions, probs = sess.run([self._predictions, self._softmax], feed_dict=feed)
-            console.log("UnigramClassifier.predict: softmax\n", probs)
             return predictions
--- test_unigram.py
+++ test_unigram.py
@@ -3,24 +3,40 @@ Run UnigramClassifier on fake data.
 """
 from classifiers import UnigramClassifier
 from utility import console
-from data_source import FakeDataSource
-import nltk
+from data_source import FakeDataSource, TweetsDataSource
+import numpy as np
 
-if __name__ == "__main__":
-    classifier = UnigramClassifier()
-    data_source = FakeDataSource()
+data_source = TweetsDataSource("data/tweets-small.txt")
+classifier = UnigramClassifier(data_source.num_labels, 2)
 
-    console.time("training")
-    classifier.train(data_source.train_inputs,
-                     data_source.train_labels,
-                     data_source.num_labels)
-    console.time_end("training")
+console.time("training")
+classifier.train(data_source.train_inputs, data_source.train_labels)
+console.time_end("training")
 
-    console.time("predicting")
-    predictions = classifier.predict(data_source.test_inputs)
-    console.time_end("predicting")
+console.time("predicting")
+train_predictions = classifier.predict(data_source.train_inputs)
+test_predictions = classifier.predict(data_source.test_inputs)
+console.time_end("predicting")
 
-    console.h1("UnigramClassifier predictions:")
-    for sentence, label in zip(data_source.test_inputs, data_source.decode_labels(predictions)):
-        console.h1(" ".join(sentence))
-        console.h1("\t->", label)
+n_train = len(data_source.train_inputs)
+train_frequencies = np.bincount(data_source.train_labels) / n_train
+train_mfc = 1-train_frequencies.max()
+train_acc = np.sum(np.equal(train_predictions, data_source.train_labels)) / n_train
+
+n_test = len(data_source.test_inputs)
+test_frequencies = np.bincount(data_source.test_labels) / n_test
+test_mfc = 1-test_frequencies.max()
+test_acc = np.sum(np.equal(test_predictions, data_source.test_labels)) / n_test
+
+console.h1("Training data:")
+console.h1("\tCount:\t{}".format(n_train))
+console.h1("\tAcc:\t{}".format(train_acc))
+console.h1("\tMfc:\t{}".format(train_mfc))
+
+console.h1("Testing data:")
+console.h1("\tCount:\t{}".format(n_test))
+console.h1("\tAcc:\t{}".format(test_acc))
+console.h1("\tMFC:\t{}".format(test_mfc))
+
+if test_mfc >= test_acc:
+    console.warn("Your model isn't better than the MFC!")
