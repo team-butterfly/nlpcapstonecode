e9927751a9a8fb199ec5ab46f46fe67e7b9ec5fb
Ollin Boer Bohan <madebyollin@gmail.com>
2017-04-18 20:08:33 -0700

    pushed the envelope (teehee)

--- tts_test.py
+++ tts_test.py
@@ -4,5 +4,5 @@ from utility import console, Emotion
 if __name__ == "__main__":
     console.h1("testing tts")
     tts = TTS()
-    tts.speak("I love programming so much", Emotion.JOY, "/Users/ollin/Desktop/happy.aif")
-    tts.speak("But I hate debugging...", Emotion.ANGER, "/Users/ollin/Desktop/anger.aif")
+    tts.speak("What a great day, sun is shining and I am smiling", Emotion.JOY, "/Users/ollin/Desktop/happy.aif")
+    tts.speak("Weekend just got even worse. I hate all of you.", Emotion.ANGER, "/Users/ollin/Desktop/anger.aif")
