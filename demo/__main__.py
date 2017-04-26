6ecec703faae10ff8805b56a6a692ad566212644
Ollin Boer Bohan <madebyollin@gmail.com>
2017-04-26 12:34:20 -0700

    demo now speaks

--- demo/__main__.py
+++ demo/__main__.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import os
 from utility import console, Emotion
+
+console.log()
+console.h1("Initializing Server")
+
 from classifiers import LstmClassifier
-import data_source
+from tts import TTS
 
 app = Flask(__name__)
 lstm = LstmClassifier()
+tts = TTS()
+
+def say(text, emotion):
+    output_path = "/tmp/" + tts.as_file_path(text) + ".aif"
+    output_path = tts.speak(text, emotion, output_path)
+    os.system("afplay '{}' || ffplay '{}' || play '{}'".format(output_path, output_path, output_path))
 
 @app.route("/")
 def main():
@@ -15,8 +26,8 @@ def classify(text):
     classifications = lstm.predict([text])
     emotion = Emotion(classifications[0]).name
     console.debug(text,"->",classifications[0],"->",emotion)
-
+    say(text, Emotion(classifications[0]))
     return "{\"" + emotion + "\" : 1.0}"
 
-if __name__ == "__main__":
-    app.run()
+console.h1("Server Ready")
+app.run()
