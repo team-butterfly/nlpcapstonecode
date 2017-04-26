03f423e548bfa7e4b00a49921c9dcfb4840ae3b7
brandonvin <brandonvin@gmail.com>
2017-04-25 19:54:41 -0700

    precision, recall, f1

--- test_models.py
+++ test_models.py
@@ -3,93 +3,102 @@ Try running the models
 """
 from classifiers import UnigramClassifier, LstmClassifier
 from data_source import TweetsDataSource
-from utility import console
+from utility import console, Emotion
 import numpy as np
+import random
 
 tweets_data_src = TweetsDataSource("data/tweets.v2.txt", random_seed=5)
 
-def assess_classifier(classifier, data_src):
-    """Use the classifier to predict, print metrics, and return accuracy on dev set"""
-    console.time("predicting")
-    train_predictions = classifier.predict(data_src.train_inputs)
-    test_predictions = classifier.predict(data_src.test_inputs)
-    console.time_end("predicting")
+def print_metrics(title, truth, predictions, mfc_class):
+    N = len(truth)
+    frequencies = np.bincount(truth) / N
+    mfc_acc = frequencies[mfc_class]
+    acc = np.equal(predictions, truth).mean()
 
+    console.h1(title)
+    console.h1("\tFrequencies: {}".format(frequencies))
+    console.h1("\tCount:\t{}".format(N))
+    console.h1("\tAcc:\t{:.4f}".format(acc))
+    console.h1("\tMfc:\t{:.4f}".format(mfc_acc))
 
-
-    n_train = len(data_src.train_inputs)
-    train_frequencies = np.bincount(data_src.train_labels) / n_train
-    mfc = train_frequencies.argmax()
-    
-    train_mfc = train_frequencies[mfc]
-    train_acc = np.equal(train_predictions, data_src.train_labels).mean()
-
-    n_test = len(data_src.test_inputs)
-    test_frequencies = np.bincount(data_src.test_labels) / n_test
-    
-    test_mfc = test_frequencies[mfc]
-    test_acc = np.equal(test_predictions, data_src.test_labels).mean()
-
-    console.h1("Training data:")
-    console.h1("\tTrain frequencies {}".format(train_frequencies))
-    console.h1("\tCount:\t{}".format(n_train))
-    console.h1("\tAcc:\t{}".format(train_acc))
-    console.h1("\tMfc:\t{}".format(train_mfc))
-
-    console.h1("Testing data:")
-    console.h1("\tTest frequencies {}".format(test_frequencies))
-    console.h1("\tCount:\t{}".format(n_test))
-    console.h1("\tAcc:\t{}".format(test_acc))
-    console.h1("\tMfc:\t{}".format(test_mfc))
-
-    if test_mfc >= test_acc:
+    if mfc_acc >= acc:
         console.warn("Your classifier isn't better than the MFC!")
 
-    return test_acc
+    for i in range(len(Emotion)):
+        console.h1("Label {} ({})".format(i, Emotion(i).name))
 
-def find_good_unk_threshold(data_src, lo, hi):
-    """Best unk_threshold is like 9 I guess"""
-    accs = {}
-    for unk in range(lo, hi+1):
-        console.h1("Trying unk_threshold={}".format(unk))
-        clsf = UnigramClassifier(data_src.num_labels, unk_threshold=unk)
-        clsf.train(
-            data_src.train_inputs,
-            data_src.train_labels,
-            max_epochs=10000)
-        test_acc = assess_classifier(clsf, data_src)
-        accs[unk] = test_acc
+        truth_is_pos = np.equal(truth, i)
+        pred_is_pos  = np.equal(predictions, i)
 
-    sorted_accs = sorted(accs.items(), key=lambda it: it[1], reverse=True) # Sort the dict by ascending values
-    best_unk, best_acc = sorted_accs[0]
-    console.log(sorted_accs)
-    console.log("The best unk_threshold was {} with accuracy {}".format(best_unk, best_acc))
+        if truth_is_pos.sum() == 0 and pred_is_pos.sum() == 0:
+            console.h1("\t[absent]")
+            continue
+
+        true_pos     = np.logical_and(truth_is_pos, pred_is_pos).sum()
+        false_pos    = np.logical_and(~truth_is_pos, pred_is_pos).sum()
+        true_neg     = np.logical_and(~truth_is_pos, ~pred_is_pos).sum()
+        false_neg    = np.logical_and(truth_is_pos, ~pred_is_pos).sum()
+
+        precision = true_pos / (true_pos + false_pos)
+        recall = true_pos / (true_pos + false_neg)
+        f1 = 2 * (precision * recall) / (precision + recall)
 
 
-def run_lstm():
-    console.h1("Running LstmClassifier")
+        console.h1("\tPrec:\t{:.4f}".format(precision))
+        console.h1("\tRec:\t{:.4f}".format(recall))
+        console.h1("\tF1:\t{:.4f}".format(f1))
 
-    class RawInputsWrapper():
-        def __init__(self, data_src):
-            self._data_src = data_src
-            
-        @property
-        def train_inputs(self):
-            return self._data_src.train_raw_inputs
 
-        @property
-        def train_labels(self):
-            return self._data_src.train_labels
 
-        @property
-        def test_inputs(self):
-            return self._data_src.test_raw_inputs
+def assess_classifier(classifier, data_src):
+    mfc_class = np.bincount(data_src.train_labels).argmax()
+    print_metrics(
+        "Training data",
+        data_src.train_labels,
+        classifier.predict(data_src.train_inputs),
+        mfc_class)
+    print_metrics(
+        "Test data",
+        data_src.test_labels,
+        classifier.predict(data_src.test_inputs),
+        mfc_class)
 
-        @property
-        def test_labels(self):
-            return self._data_src.test_labels
 
-    assess_classifier(LstmClassifier(), RawInputsWrapper(tweets_data_src))
+class RawInputsWrapper():
+    def __init__(self, data_src):
+        self._data_src = data_src
+        
+    @property
+    def train_inputs(self):
+        return self._data_src.train_raw_inputs
+
+    @property
+    def train_labels(self):
+        return self._data_src.train_labels
+
+    @property
+    def test_inputs(self):
+        return self._data_src.test_raw_inputs
+
+    @property
+    def test_labels(self):
+        return self._data_src.test_labels
+
 
 if __name__ == "__main__":
-    run_lstm()
+    for label in tweets_data_src.test_labels:
+        assert Emotion(label).value == label
+    for label in tweets_data_src.train_labels:
+        assert Emotion(label).value == label
+
+    console.h1("UnigramClassifier")
+    unigram = UnigramClassifier(len(Emotion))
+    unigram.train(tweets_data_src.train_inputs, tweets_data_src.train_labels, max_epochs=10000)
+    assess_classifier(unigram, tweets_data_src)
+    
+    console.h1("-" * 80)
+    console.h1("LstmClassifier")
+    lstm_input = RawInputsWrapper(tweets_data_src)
+    assess_classifier(LstmClassifier(), lstm_input)
+
+
